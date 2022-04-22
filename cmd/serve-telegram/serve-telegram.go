be3919f934da55d1c63eec6ee25c38ba0893ccbe
Maxime Amodei <maxime.amodei@gmail.com>
2022-04-22 10:06:18 +0200

    add authentication to make the bot private

--- cmd/serve-telegram/serve-telegram.go
+++ cmd/serve-telegram/serve-telegram.go
@@ -11,9 +11,12 @@ import (
 	"syscall"
 	"time"
 
+	badger "github.com/dgraph-io/badger/v3"
 	"github.com/maxime915/waker/pkg/waker"
 	telegram "gopkg.in/tucnak/telebot.v3"
 )
+
+const user_key = "user_key"
 
 type VerbArguments struct {
 	Token     string `goptions:"--token, obligatory, description='Telegram API token for the bot'"`
@@ -21,11 +24,28 @@ type VerbArguments struct {
 	Killable  bool   `goptions:"--killable, description='Creates a /kill route to terminate the server'"`
 	Target    string `goptions:"-t, --target, obligatory, description='MAC address to wake up'"`
 	Broadcast string `goptions:"-b, --broadcast, description='Broadcast address & port to send the packet'"`
+	StorePath string `goptions:"--store-path, description='Path to store bot data'"`
+	InMemory  bool   `goptions:"--in-memory, description='Do not use disk to retain bot data'"`
 }
 
 func (va VerbArguments) Execute() {
 
 	flag.Parse()
+
+	if va.InMemory != (len(va.StorePath) == 0) {
+		log.Fatalf("exactly only one of --store-path='%v' , --in-memory='%v' must be set", va.StorePath, va.InMemory)
+	}
+
+	if !va.InMemory && len(va.StorePath) == 0 {
+		log.Fatalf("only one of --store-path='%v' , --in-memory='%v' must be set", va.StorePath, va.InMemory)
+	}
+
+	options := badger.DefaultOptions(va.StorePath).WithInMemory(va.InMemory)
+	db, err := badger.Open(options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	packet, err := waker.NewMagicPacket(va.Target)
 	if err != nil {
@@ -54,6 +74,59 @@ func (va VerbArguments) Execute() {
 		<-c
 		close(interrupted)
 	}()
+
+	// Add authentication middleware: only one user has access to the bot
+	bot.Use(func(next telegram.HandlerFunc) telegram.HandlerFunc {
+		return func(ctx telegram.Context) error {
+			chat_id := fmt.Sprint(ctx.Chat().ID)
+			authenticated := false
+
+			err := db.Update(func(txn *badger.Txn) error {
+
+				// fetch data
+				fetched_id, err := txn.Get([]byte(user_key))
+
+				// no user -> save this as the user
+				if err == badger.ErrKeyNotFound {
+					// insert it
+					err = txn.Set([]byte(user_key), []byte(chat_id))
+					if err != nil {
+						return err
+					}
+
+					// log the user if no error
+					authenticated = true
+					return nil
+				}
+
+				// compare the chat with the saved user
+				return fetched_id.Value(func(val []byte) error {
+					// no value found: no user will ever be able to login
+					if val == nil {
+						log.Fatal(fmt.Errorf("no value found for key %v", user_key))
+					}
+
+					authenticated = string(val) == chat_id
+
+					return nil
+				})
+			})
+
+			if err != nil {
+				log.Print(err)
+				return nil
+			}
+
+			if authenticated {
+				return next(ctx)
+			}
+
+			log.Printf("unauthorized loggin attempt (chat id: %v, username: %v, firstname: %v, lastname: %v)", chat_id, ctx.Chat().Username, ctx.Chat().FirstName, ctx.Chat().LastName)
+
+			_, err = bot.Send(ctx.Sender(), "Sorry, this bot is private.")
+			return err
+		}
+	})
 
 	bot.Handle("/info", func(c telegram.Context) error {
 		msg := fmt.Sprintf("Killable: %v\nTarget: %v\nBroadcast: %v\n", va.Killable, va.Target, va.Broadcast)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/maxime915/waker
 go 1.16
 
 require (
+	github.com/dgraph-io/badger/v3 v3.2103.2
 	github.com/stretchr/testify v1.7.0
 	github.com/voxelbrain/goptions v0.0.0-20180630082107-58cddc247ea2
 	gopkg.in/tucnak/telebot.v3 v3.0.0-20210913074324-90e7deaa7aa6
